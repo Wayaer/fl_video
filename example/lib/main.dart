b6d7324fd09b2344eeed8eecea4d9b8494be2178
Wayaer <wayaer@foxmail.com>
2023-10-23 11:52:05 +0800

    no message

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,21 +1,13 @@
 import 'package:fl_video/fl_video.dart';
 import 'package:flutter/material.dart';
 
-class AppTheme {
-  static final light = ThemeData(
-      brightness: Brightness.light,
-      disabledColor: Colors.grey.shade400,
-      visualDensity: VisualDensity.adaptivePlatformDensity);
-
-  static final dark = ThemeData(
-      brightness: Brightness.dark,
-      disabledColor: Colors.grey.shade400,
-      visualDensity: VisualDensity.adaptivePlatformDensity);
-}
-
 void main() {
-  runApp(const MaterialApp(
-      debugShowCheckedModeBanner: false, title: 'FlVideo', home: _HomePage()));
+  runApp(MaterialApp(
+      theme: ThemeData.light(useMaterial3: true),
+      darkTheme: ThemeData.dark(useMaterial3: true),
+      debugShowCheckedModeBanner: false,
+      title: 'FlVideo',
+      home: const _HomePage()));
 }
 
 class _HomePage extends StatefulWidget {
@@ -25,30 +17,67 @@ class _HomePage extends StatefulWidget {
   State<_HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<_HomePage> {
-  late final VideoPlayerController _videoPlayerController1 =
-      VideoPlayerController.networkUrl(Uri.parse(
-          'https://assets.mixkit.co/videos/preview/mixkit-daytime-city-traffic-aerial-view-56-large.mp4'));
-  late final VideoPlayerController _videoPlayerController2 =
-      VideoPlayerController.networkUrl(Uri.parse(
-          'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1226-large.mp4'));
-  FlVideoPlayerController? _controller;
+class _HomePageState extends State<_HomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController controller;
+
+  final controls = [
+    const _MaterialControlsVideoPlayer(),
+    const _CupertinoControlsVideoPlayer()
+  ];
 
   @override
   void initState() {
     super.initState();
-    initializePlayer(_videoPlayerController1);
+    controller = TabController(length: 2, vsync: this);
   }
 
   @override
-  void dispose() {
-    _controller?.dispose();
-    super.dispose();
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Fl Video Player')),
+        body: SafeArea(
+          bottom: true,
+          child: Column(children: [
+            Expanded(
+                child: TabBarView(controller: controller, children: controls)),
+            const SizedBox(height: 40),
+            Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+              ElevatedText(
+                  onPressed: () {
+                    controller.animateTo(0);
+                  },
+                  text: "MaterialControls"),
+              ElevatedText(
+                  onPressed: () {
+                    controller.animateTo(1);
+                  },
+                  text: 'CupertinoControls')
+            ]),
+            const SizedBox(height: 40),
+          ]),
+        ));
   }
+}
 
-  void initializePlayer(VideoPlayerController videoPlayerController) {
+class _CupertinoControlsVideoPlayer extends StatefulWidget {
+  const _CupertinoControlsVideoPlayer();
+
+  @override
+  State<_CupertinoControlsVideoPlayer> createState() =>
+      _CupertinoControlsVideoPlayerState();
+}
+
+class _CupertinoControlsVideoPlayerState
+    extends State<_CupertinoControlsVideoPlayer> {
+  late FlVideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
     _controller = FlVideoPlayerController(
-        videoPlayerController: videoPlayerController,
+        videoPlayerController: VideoPlayerController.networkUrl(Uri.parse(
+            'https://assets.mixkit.co/videos/preview/mixkit-a-girl-blowing-a-bubble-gum-at-an-amusement-park-1226-large.mp4')),
         autoPlay: true,
         looping: true,
         overlay: const IgnorePointer(
@@ -58,55 +87,35 @@ class _HomePageState extends State<_HomePage> {
         placeholder: const Center(
             child: Text('placeholder',
                 style: TextStyle(color: Colors.red, fontSize: 20))),
-        controls: videoPlayerController == _videoPlayerController1
-            ? MaterialControls(
-                hideDuration: const Duration(seconds: 5),
-                enablePlay: true,
-                enableFullscreen: true,
-                enableSpeed: true,
-                enableVolume: true,
-                enableSubtitle: true,
-                enablePosition: true,
-                enableBottomBar: true,
-                onTap: (FlVideoTapEvent event,
-                    FlVideoPlayerController controller) {
-                  debugPrint(event.toString());
-                },
-                onDragProgress:
-                    (FlVideoDragProgressEvent event, Duration duration) {
-                  debugPrint('$event===$duration');
-                })
-            : CupertinoControls(
-                hideDuration: const Duration(seconds: 5),
-                enableSpeed: true,
-                enableSkip: true,
-                enableSubtitle: true,
-                enableFullscreen: true,
-                enableVolume: true,
-                enablePlay: true,
-                enableBottomBar: true,
-                onTap: (FlVideoTapEvent event,
-                    FlVideoPlayerController controller) {
-                  debugPrint(event.toString());
-                },
-                onDragProgress:
-                    (FlVideoDragProgressEvent event, Duration duration) {
-                  debugPrint('$event===$duration');
-                },
-                remainingBuilder: (String position) {
-                  return Padding(
-                      padding: const EdgeInsets.fromLTRB(0, 6, 6, 6),
-                      child: Text(position,
-                          style: const TextStyle(
-                              fontSize: 16, color: Colors.red)));
-                },
-                positionBuilder: (String position) {
-                  return Padding(
-                      padding: const EdgeInsets.fromLTRB(6, 6, 0, 6),
-                      child: Text(position,
-                          style: const TextStyle(
-                              fontSize: 16, color: Colors.lightBlue)));
-                }),
+        controls: CupertinoControls(
+            hideDuration: const Duration(seconds: 5),
+            enableSpeed: true,
+            enableSkip: true,
+            enableSubtitle: true,
+            enableFullscreen: true,
+            enableVolume: true,
+            enablePlay: true,
+            enableBottomBar: true,
+            onTap: (FlVideoTapEvent event, FlVideoPlayerController controller) {
+              debugPrint(event.toString());
+            },
+            onDragProgress:
+                (FlVideoDragProgressEvent event, Duration duration) {
+              debugPrint('$event===$duration');
+            },
+            remainingBuilder: (String position) {
+              return Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 6, 6, 6),
+                  child: Text(position,
+                      style: const TextStyle(fontSize: 16, color: Colors.red)));
+            },
+            positionBuilder: (String position) {
+              return Padding(
+                  padding: const EdgeInsets.fromLTRB(6, 6, 0, 6),
+                  child: Text(position,
+                      style: const TextStyle(
+                          fontSize: 16, color: Colors.lightBlue)));
+            }),
         subtitle: Subtitles([
           Subtitle(
               index: 0,
@@ -123,40 +132,82 @@ class _HomePageState extends State<_HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: const Text('Fl Video Player')),
-        body: SafeArea(
-          bottom: true,
-          child: Column(children: <Widget>[
-            Expanded(child: FlVideoPlayer(controller: _controller!)),
-            const SizedBox(height: 40),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  ElevatedText(
-                      onPressed: () async {
-                        if (_controller!.videoPlayerController !=
-                            _videoPlayerController1) {
-                          await _controller!.dispose();
-                          initializePlayer(_videoPlayerController1);
-                          setState(() {});
-                        }
-                      },
-                      text: "MaterialControls"),
-                  ElevatedText(
-                      onPressed: () async {
-                        if (_controller!.videoPlayerController !=
-                            _videoPlayerController2) {
-                          await _controller!.dispose();
-                          initializePlayer(_videoPlayerController2);
-                          setState(() {});
-                        }
-                      },
-                      text: 'CupertinoControls')
-                ]),
-            const SizedBox(height: 40),
-          ]),
-        ));
+    return FlVideoPlayer(controller: _controller);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+}
+
+class _MaterialControlsVideoPlayer extends StatefulWidget {
+  const _MaterialControlsVideoPlayer();
+
+  @override
+  State<_MaterialControlsVideoPlayer> createState() =>
+      _MaterialControlsVideoPlayerState();
+}
+
+class _MaterialControlsVideoPlayerState
+    extends State<_MaterialControlsVideoPlayer> {
+  late FlVideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = FlVideoPlayerController(
+        videoPlayerController: VideoPlayerController.networkUrl(Uri.parse(
+            'https://assets.mixkit.co/videos/preview/mixkit-daytime-city-traffic-aerial-view-56-large.mp4')),
+        autoPlay: true,
+        looping: true,
+        overlay: const IgnorePointer(
+            child: Center(
+                child: Text('overlay',
+                    style: TextStyle(color: Colors.lightBlue, fontSize: 20)))),
+        placeholder: const Center(
+            child: Text('placeholder',
+                style: TextStyle(color: Colors.red, fontSize: 20))),
+        controls: MaterialControls(
+            hideDuration: const Duration(seconds: 5),
+            enablePlay: true,
+            enableFullscreen: true,
+            enableSpeed: true,
+            enableVolume: true,
+            enableSubtitle: true,
+            enablePosition: true,
+            enableBottomBar: true,
+            onTap: (FlVideoTapEvent event, FlVideoPlayerController controller) {
+              debugPrint(event.toString());
+            },
+            onDragProgress:
+                (FlVideoDragProgressEvent event, Duration duration) {
+              debugPrint('$event===$duration');
+            }),
+        subtitle: Subtitles([
+          Subtitle(
+              index: 0,
+              start: Duration.zero,
+              end: const Duration(seconds: 10),
+              text: 'No.1 subtitle'),
+          Subtitle(
+              index: 0,
+              start: const Duration(seconds: 10),
+              end: const Duration(seconds: 20),
+              text: 'No.2 subtitle)'),
+        ]));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlVideoPlayer(controller: _controller);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
   }
 }
 
